0c02eaaeb6d6dd2c504dff86ba74179278a2ec64
Yousef Tarek <68440801+Cloudness354@users.noreply.github.com>
2022-10-26 14:07:23 -0400

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>AlTarek1 Store</title>
       </Head>
       <header>
         <Navbar />
